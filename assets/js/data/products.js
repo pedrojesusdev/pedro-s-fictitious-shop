2fe317afd0472215a58309d827b91bca7fa09e20
pedrojesusdev <pa04052007@gmail.com>
2026-01-07 02:03:11 -0300

    Adding more products to the catalog

--- assets/js/data/products.js
+++ assets/js/data/products.js
@@ -1,7 +1,7 @@
 export const products = [
   {
     id: 1,
-    name: "Air Zoom Alpha",
+    name: "Air Zoom Alphafly next% 2",
     price: 799.9,
     image: "/assets/images/products/air-zoom-alpha.png",
     category: "Running"
@@ -9,15 +9,50 @@ export const products = [
   {
     id: 2,
     name: "React Infinity",
-    price: 699.9,
+    price: 854.99,
     image: "/assets/images/products/react-infinity.png",
     category: "Training"
   },
   {
     id: 3,
     name: "Air Force One",
-    price: 649.9,
+    price: 759.99,
     image: "/assets/images/products/air-force-one.png",
     category: "Lifestyle"
+  },
+  {
+    id: 4,
+    name: "Air Max DN",
+    price: 1234.99,
+    image: "/assets/images/products/air-max-dn.png",
+    category: "Lifestyle"
+  },
+  {
+    id: 5,
+    name: "Air Max 95 OG",
+    price: 1329.99,
+    image: "/assets/images/products/air-max-95.png",
+    category: "Lifestyle"
+  },
+  {
+    id: 6,
+    name: "Air Max 97",
+    price: 1234.99,
+    image: "/assets/images/products/air-max-97.png",
+    category: "Lifestyle"
+  },
+  {
+    id: 7,
+    name: "Structure 26",
+    price: 949.99,
+    image: "/assets/images/products/structure-26.png",
+    category: "Running"
+  },
+  {
+    id: 8,
+    name: "MC Trainer 3",
+    price: 398.99,
+    image: "/assets/images/products/mc-trainer-3.png",
+    category: "Training"
   }
 ];
